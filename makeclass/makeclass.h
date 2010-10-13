9cf51aaa36f67669bbb0b4eb016b42e2dae78cb4
ian <Ian Good icgood@gmail.com>
2010-10-13 11:46:10 -0400

    Changed function prefix to rhelp_

--- makeclass/makeclass.h
+++ makeclass/makeclass.h
@@ -1,13 +1,13 @@
-#ifndef __RATCHET_MAKECLASS_H
-#define __RATCHET_MAKECLASS_H
+#ifndef __rhelp_MAKECLASS_H
+#define __rhelp_MAKECLASS_H
 
 #include <lua.h>
 #include "misc.h"
 
-#define ratchet_setclassfield ratchet_rawsetfield
+#define rhelp_setclassfield rhelp_rawsetfield
 
-void ratchet_newclass (lua_State *L, const char *name, const luaL_Reg *meths, const luaL_Reg *funcs);
-int ratchet_makeclass (lua_State *L);
+void rhelp_newclass (lua_State *L, const char *name, const luaL_Reg *meths, const luaL_Reg *funcs);
+int rhelp_makeclass (lua_State *L);
 
 #endif
 // vim:foldmethod=marker:ai:ts=4:sw=4:
