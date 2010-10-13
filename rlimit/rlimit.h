3c603f72014b4902837b6bed2b7f059bf4cdd1ee
ian <Ian Good icgood@gmail.com>
2010-10-13 11:47:44 -0400

    Removed references to luah

--- rlimit/rlimit.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LUAH_RLIMIT_H
-#define __LUAH_RLIMIT_H
-
-#include <lua.h>
-
-int luaopen_luah_rlimit (lua_State *L);
-
-#endif
-// vim:foldmethod=marker:ai:ts=4:sw=4:
--- xml/xml.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LUAH_XML_H
-#define __LUAH_XML_H
-
-#include <lua.h>
-
-int luaopen_luah_xml (lua_State *L);
-
-#endif
-// vim:foldmethod=marker:ai:ts=4:sw=4:
